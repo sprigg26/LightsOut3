b4b3c2b720443814bdba3e19ee19e4778e661acb
sprigg26 <sprigg26@up.edu>
2023-10-08 01:23:59 -0700

    fixed win conditions and added reset button

--- app/src/main/java/com/example/lightsout3/MainActivity.java
+++ app/src/main/java/com/example/lightsout3/MainActivity.java
@@ -2,13 +2,11 @@ package com.example.lightsout3;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.gridlayout.widget.GridLayout;
-
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 import java.util.Random;
 
 
@@ -21,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         androidx.gridlayout.widget.GridLayout gridLayout = findViewById(R.id.gridLayout);
+        Button resetButton = findViewById(R.id.button2);
+
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
                 buttons[i][j] = new Button(this);
@@ -53,20 +53,26 @@ public class MainActivity extends AppCompatActivity {
 
                         // Check if the player has won (all buttons are black)
                         if (isGameWon()) {
-                            //System.out.println("Game Won!!!!!");
                             gridLayout.setBackgroundColor(Color.GREEN);
                         }
                     }
 
                 });
 
-                // Add the button to the GridLayout
+                //add the buttons to the gridLayout
                 gridLayout.addView(buttons[i][j]);
+
+                resetButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // Handle the reset button click event here
+                        resetGrid(v); // Call your resetGrid method or any other logic
+                        gridLayout.setBackgroundColor(Color.WHITE);
+
+                    }
+                });
             }
         }
-
-
-
     }
 
     //helper method to change the button colors
@@ -86,19 +92,21 @@ public class MainActivity extends AppCompatActivity {
 
     // Helper method to toggle the colors of adjacent buttons
     private void toggleAdjacentButtons(int row, int col) {
+        //acquired help from online resources for this method (stackoverflow and forums)
         int[][] directions = { {0, -1}, {-1, 0}, {0, 1}, {1, 0} }; // Up, Left, Down, Right
 
         for (int[] dir : directions) {
             int newRow = row + dir[0];
             int newCol = col + dir[1];
 
-            if (isValidPosition(newRow, newCol)) {
+            if (isValidPosition(newRow, newCol))
+            {
                 toggleButtonColor(newRow, newCol);
             }
         }
     }
 
-    // Helper method to check if a position is valid (within the grid)
+    //helper method to prevent crashing by forcing the positions to be within the 5x5 grid
     private boolean isValidPosition(int row, int col) {
         return row >= 0 && row < 5 && col >= 0 && col < 5;
     }
@@ -107,7 +115,9 @@ public class MainActivity extends AppCompatActivity {
     private boolean isGameWon() {
         for (int row = 0; row < 5; row++) {
             for (int col = 0; col < 5; col++) {
-                if (buttons[row][col].getBackground().equals(Color.WHITE)) {
+                ColorDrawable buttonColor = (ColorDrawable) buttons[row][col].getBackground();
+                if (buttonColor.getColor() != Color.BLACK)
+                {
                     return false;
                 }
             }
@@ -117,7 +127,25 @@ public class MainActivity extends AppCompatActivity {
 
     private int getRandomColor() {
         Random random = new Random();
-        return random.nextBoolean() ? Color.BLACK : Color.WHITE;
+        int randomNumber = random.nextInt(2);
+        if(randomNumber == 1)
+        {
+            return Color.BLACK;
+        }
+        else
+        {
+            return Color.WHITE;
+        }
+    }
+
+
+    public void resetGrid(View v) {
+        for (int row = 0; row < 5; row++) {
+            for (int col = 0; col < 5; col++) {
+                buttons[row][col].setBackgroundColor(getRandomColor());
+            }
+        }
+
     }
 
 }
